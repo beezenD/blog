835a361d69de2b8d4a52980f63f9b64a75a5ea39
beezen <bizhen.dong@crgecent.com>
2020-08-19 16:19:04 +0800

    Taro转快应用的那些事

--- scripts/doc.js
+++ scripts/doc.js
@@ -4,7 +4,7 @@ const path = require('path')
 const targetDir = path.resolve(__dirname, '../doc') // 目标文件夹
 const destFile = path.resolve(__dirname, '../README.md') // 输出文件
 
-const entries = digo.readDir(targetDir).filter((name) => name != 'images')
+const entries = digo.readDir(targetDir).filter((name) => name != 'images').reverse()
 
 let doc_tpl = `# beezen 的日常记录\n\n[beezen博客地址](http://www.dongbizhen.com)\n\n## 目录`
 

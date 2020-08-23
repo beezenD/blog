af34dab71e751c1ed6b7d59234cf772711019059
beezen <bizhen.dong@crgecent.com>
2020-08-23 22:20:04 +0800

    20200630-Taro转快应用的那些事（二）.md文章更新

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,7 @@
 const digo = require('digo')
 const path = require('path')
 const inquirer = require('inquirer')
+const YAML = require('js-yaml')
 
 const draftDir = path.resolve(__dirname, '../draft') // 暂存文件夹
 const docDir = path.resolve(__dirname, '../doc') // 发布文件夹
@@ -27,10 +28,12 @@ inquirer
     // 选中文章将会同步到 doc 发布文件夹
     answers.article.forEach((e) => {
       // 过滤文件头
-      digo.copyFile(
-        path.resolve(draftDir, entries[e]),
-        path.resolve(docDir, entries[e])
-      )
+      const fileStr = digo
+        .readFile(path.resolve(draftDir, entries[e]))
+        .toString()
+      const newstr = fileStr.replace(/---([\w\W]*)---/, '')
+      // 文件写入
+      digo.writeFile(path.resolve(docDir, entries[e]), newstr)
       console.log(`${entries[e]} 已同步到 doc 目录`)
       commitMsg.push(entries[e])
     })
@@ -47,6 +50,8 @@ inquirer
 
     // git 智能提交
     digo.exec(
-      `git add . && git commit -am "${commitMsg.join(',')}文章更新" && git push -f`
+      `git add . && git commit -am "${commitMsg.join(
+        ','
+      )}文章更新" && git push -f`
     )
   })
